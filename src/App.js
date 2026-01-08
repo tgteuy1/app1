744867d1e93b5caa80de1d5c99db60c6160ae388
tgteuy1 <tgteuy1@gmail.com>
2026-01-08 11:47:06 +0700

    Add Event data Start/Stop

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
-import { Calculator2 } from './calculator';
+import { EventData2 } from './event-data';
+
 
 
 function App() {
-  return <Calculator2></Calculator2>
+  return <EventData2></EventData2>
     
 }
 
