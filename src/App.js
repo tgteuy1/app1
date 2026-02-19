c06f4bacf8de4e277ce3087414aaca14f5693bbd
tgteuy1 <tgteuy1@gmail.com>
2026-02-19 11:47:03 +0700

    inputText

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import Router3 from './router3'
+import FormText from './form-text'
 
 export default function App() {
   return (
-    <Router3></Router3>
+    <FormText></FormText>
     )
 }
